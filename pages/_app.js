bd23bca9d61b6c7d8e3cd3866a57a3571b768960
Fukuda Naoto <naoto.fukuda1217@gmail.com>
2021-01-10 18:05:49 +0900

    add Chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
-export default MyApp
+export default App
