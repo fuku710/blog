711e2b6bd057161f8bfdf1c168d1798940886da3
Fukuda Naoto <naoto.fukuda1217@gmail.com>
2021-01-16 23:43:29 +0900

    change color

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,8 @@
 import { Box, Heading, Text, Divider, Link } from '@chakra-ui/react'
 export default function Header() {
   return (
-    <Box as="header" shadow="md" w="full">
-      <Heading as="h1" m="2">
+    <Box as="header" shadow="md" w="full" bg="blue.800">
+      <Heading as="h1" m="2" color="white">
         {process.env.title}
       </Heading>
     </Box>
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 import Layout from '../components/Layout'
 import Header from '../components/Header'
 
+const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        bgColor: 'gray.50',
+      },
+    },
+  },
+})
+
 function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Layout>
         <Header></Header>
         <Component {...pageProps} />
